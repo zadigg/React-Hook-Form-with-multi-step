9eca657012d667b0e0f22fc00d66c188d86b213c
zadigg <62830716+zadigg@users.noreply.github.com>
2022-05-05 15:44:46 +0300

    SVG syntax solved

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
 
   const submitForm = (values) => {
     window.alert(JSON.stringify(values, null, 2));
+    // console.log(values);
     completeFormStep();
   };
 
@@ -102,15 +103,14 @@ function App() {
                     <button onClick={goToPreviousStep} type="button">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-5 w-5"
                         viewBox="0 0 20 20"
                         fill="currentColor"
                         className="w-6 text-gray-400 hover:text-gray-600 mr-2"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         />
                       </svg>
                     </button>
